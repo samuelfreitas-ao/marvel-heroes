8c581de1fa6c22ee65c8e980aa74bbc1a73f5d65
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 12:39:51 +0100

    style: import global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AppRoutes } from './presentation/routes'
+import { GlobalStyle } from './presentation/styles/global'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <AppRoutes />
   </React.StrictMode>
 )
