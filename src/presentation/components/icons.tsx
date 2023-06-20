d7e85e2d3baf86466ffea4501c91ac3380218903
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 20:38:36 +0100

    feat: add IconSpinner

--- src/presentation/components/icons.tsx
+++ src/presentation/components/icons.tsx
@@ -1,5 +1,7 @@
 import { IconBaseProps } from 'react-icons'
 
 import { IoMdSearch } from 'react-icons/io'
+import { AiOutlineLoading3Quarters } from 'react-icons/ai'
 
 export const IconSearch = (props: IconBaseProps) => <IoMdSearch {...props} />
+export const IconSpinner = (props: IconBaseProps) => <AiOutlineLoading3Quarters {...props} />
