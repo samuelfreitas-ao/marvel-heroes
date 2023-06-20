8cb96a87be4967f8e39a405564c694ca86981d90
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 17:46:41 +0100

    chore: remove unused code

--- src/presentation/components/search/index.tsx
+++ src/presentation/components/search/index.tsx
@@ -1,10 +1,6 @@
-import { ReactNode } from 'react'
 import { Container, Input, Button, InputContent } from './styled'
 import { IconSearch } from '..'
 
-type SearchHeroesProps = {
-  children: ReactNode
-}
 export const SearchHeroes = () => {
   return (
     <Container>
