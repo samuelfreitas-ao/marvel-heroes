6065338f373e80bdcfb193a127ec1a4d21f8b129
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 14:37:53 +0100

    style: header min-height

--- src/presentation/components/header/styled.ts
+++ src/presentation/components/header/styled.ts
@@ -9,7 +9,7 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  min-height: 300px;
+  min-height: 304px;
   padding: 24px;
   padding-bottom: 64px;
 `
