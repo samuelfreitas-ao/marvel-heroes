a00c5579bad9004bbf3e3d93f2c85486a8dd598a
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 20:41:29 +0100

    refactor: HeroItem data

--- src/presentation/pages/home/components/hero/hero-item.tsx
+++ src/presentation/pages/home/components/hero/hero-item.tsx
@@ -1,10 +1,23 @@
-import { Item } from './styled'
+import { Item, ItemImage, ItemTitle } from './styled'
 import { Heroe } from '../../../../../domain/models'
 
 type HeroItemProps = {
-  hero: Heroe
+	hero: Heroe
 }
 
 export function HeroItem({ hero }: HeroItemProps) {
-  return <Item>{hero.name}</Item>
+	const { extension, path } = hero.thumbnail
+	const imgUrl = `${path}/standard_fantastic.${extension}`
+	let imgUnavailable = ''
+	if (imgUrl.includes('image_not_available')) {
+		imgUnavailable = 'true'
+	}
+	return (
+		<Item>
+			<ItemImage src={imgUrl} unavailable={imgUnavailable} />
+			<ItemTitle>
+				{hero.name}
+			</ItemTitle>
+		</Item>
+	)
 }
--- src/presentation/pages/home/components/hero/styled.ts
+++ src/presentation/pages/home/components/hero/styled.ts
@@ -6,7 +6,31 @@ export const List = styled.ul`
   grid-gap: 16px;
   list-style: none;
   padding: 0;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @media (max-width: 900px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
 `
 export const Item = styled.li`
-  background-color: red;
+  position: relative;
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+  display: flex;
+  &:hover {
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.5);
+  }
+`
+export const ItemImage = styled.img<{ unavailable?: string }>`
+  opacity: ${(props) => (props?.unavailable ? 0.6 : 0.8)};
+`
+export const ItemTitle = styled.div`
+  background-color: rgba(90, 126, 118, 0.8);
+  color: #fff;
+  font-size: 24px;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 `
