dc70979a94799ae2218a13b23c36aa950026299e
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 14:38:46 +0100

    style: home page

--- src/presentation/pages/home/styled.ts
+++ src/presentation/pages/home/styled.ts
@@ -8,10 +8,21 @@ export const Container = styled.div`
 export const Body = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 8px;
+  background-color: #ffffff;
+  padding: 24px;
+  border-radius: 32px;
+  margin-top: -80px;
+  margin-left: 24px;
+  margin-right: 24px;
 `
 
 export const Title = styled.div`
-  display: flex;
+  background-color: #363838;
+  color: #ffffff;
+  padding: 8px;
+  text-align: center;
+  font-size: 32px;
 `
 
 export const List = styled.ul`
