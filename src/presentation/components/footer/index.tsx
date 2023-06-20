3db445ffb911974bdace5f3abe52b7de48205fb4
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 22:57:38 +0100

    feat: add social midea info on footer

--- src/presentation/components/footer/index.tsx
+++ src/presentation/components/footer/index.tsx
@@ -1,9 +1,38 @@
-import { Container } from "./styled"
+import { IconFacebook, IconGithub, IconLinkedin, IconWhatsapp } from ".."
+import { getPersonalInfo } from "../../../utils/data"
+import { Container, SocialItem, SocialList } from "./styled"
+
+const { github, facebook, linkedin, whatsAppLink } = getPersonalInfo()
 
 export function Footer() {
+	const data = [
+		{
+			link: github,
+			icon: <IconGithub size={24} />
+		},
+		{
+			link: linkedin,
+			icon: <IconLinkedin size={24} />
+		},
+		{
+			link: whatsAppLink,
+			icon: <IconWhatsapp size={24} />
+		},
+		{
+			link: facebook,
+			icon: <IconFacebook size={24} />
+		}
+	]
 	return (
 		<Container>
-			Todos os direitos à <a href="//github.com/samuelfreitas-ao" target="_blank">Samuel Freitas</a>
+			<div>Todos os direitos à <a href="//github.com/samuelfreitas-ao" target="_blank">Samuel Freitas</a></div>
+			<SocialList>
+				{data.map(item => (
+					<SocialItem key={item.link}>
+						<a href={item.link} target="_blank">{item.icon}</a>
+					</SocialItem>
+				))}
+			</SocialList>
 		</Container>
 	)
 }
