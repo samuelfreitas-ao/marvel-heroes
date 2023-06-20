06572c63c64993d758350386834d5d30e7be937d
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 14:39:06 +0100

    style: body style

--- src/presentation/styles/global.ts
+++ src/presentation/styles/global.ts
@@ -6,6 +6,12 @@ export const GlobalStyle = createGlobalStyle`
 		margin: 0;
 	}
 
+	body{
+		background-color: #6F6969;
+		max-width: 1530px;
+		margin: auto;
+	}
+
 	:not(h1, h2, h3, strong, b){
 		font-weight: 500;
 		font-size: 16px;
