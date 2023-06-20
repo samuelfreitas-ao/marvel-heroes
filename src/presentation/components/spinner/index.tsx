69d7eebaa375d912d49e4fa2dded0fd67bf33018
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 20:39:32 +0100

    feat: add Spinner component

---
+++ src/presentation/components/spinner/index.tsx
@@ -0,0 +1,12 @@
+import { IconSpinner } from ".."
+import { Container, SpinnerItem } from "./styled"
+
+export function Spinner() {
+	return (
+		<Container>
+			<SpinnerItem>
+				<IconSpinner />
+			</SpinnerItem>
+		</Container>
+	)
+}
---
+++ src/presentation/components/spinner/styled.ts
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components'
+import { animateSpin } from '../../styles'
+
+export const Container = styled.span`
+  display: inline-block;
+`
+
+export const SpinnerItem = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${animateSpin} 1s linear infinite;
+`
