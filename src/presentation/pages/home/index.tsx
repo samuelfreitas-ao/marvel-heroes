29417898aae217f71bb856008ccffb3c4299eb0f
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 20:42:06 +0100

    feat: add loading in home

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,44 +1,49 @@
 import { useCallback, useEffect, useState } from 'react'
 
-import { Header } from '../../components'
+import { Header, Loading, Spinner } from '../../components'
 import { HeroList } from './components'
 import { Body, Container, Title } from './styled'
 import { Heroe } from '../../../domain/models'
 import { LoadHeroes } from '../../../domain/usecases'
 
 type HomeProps = {
-  loadHeroes: LoadHeroes
+	loadHeroes: LoadHeroes
 }
 
 export function Home({ loadHeroes }: HomeProps) {
-  const [heroes, setHeroes] = useState<Heroe[]>([])
+	const [heroes, setHeroes] = useState<Heroe[]>([])
+	const [isLoding, setIsLoading] = useState(true)
 
-  const fetchHeroes = useCallback(async () => {
-    try {
-      const httpResponse = await loadHeroes.loadAll()
-      setHeroes(httpResponse)
-    } catch (error) {
-      console.log('Error', error)
-    }
+	const fetchHeroes = useCallback(async () => {
+		try {
+			const httpResponse = await loadHeroes.loadAll()
+			setHeroes(httpResponse)
+		} catch (error) {
+			console.log('Error', error)
+		} finally {
+			setIsLoading(false)
+		}
 	}, [loadHeroes])
 
-  useEffect(() => {
-    let loaded = true
-    if (loaded) {
-      fetchHeroes()
-    }
-    return () => {
-      loaded = false
-    }
+	useEffect(() => {
+		let loaded = true
+		if (loaded) {
+			fetchHeroes()
+		}
+		return () => {
+			loaded = false
+		}
 	}, [fetchHeroes])
 
-  return (
-    <Container>
-      <Header />
-      <Body>
-        <Title>Personagens</Title>
-        <HeroList heroes={heroes} />
-      </Body>
-    </Container>
-  )
+	return (
+		<Container>
+			<Header />
+			<Body>
+				<Title>Personagens {isLoding && <Spinner />}</Title>
+				{isLoding ? <Loading data='Carregando personagens...' /> :
+					<HeroList heroes={heroes} />
+				}
+			</Body>
+		</Container>
+	)
 }
