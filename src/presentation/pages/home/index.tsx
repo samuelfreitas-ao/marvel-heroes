de4d30fa28f40ae6a782e18143afc0fc4418d328
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 16:47:48 +0100

    feat: inject LoadHeroes dependency in home page

--- src/presentation/pages/home/index.tsx
+++ src/presentation/pages/home/index.tsx
@@ -1,22 +1,38 @@
-import { Header } from '../../components'
-import { Body, Container, Item, List, Title } from './styled'
+import { useEffect, useState } from 'react'
 
-export function Home() {
+import { Header } from '../../components'
+import { HeroList } from './components'
+import { Body, Container, Title } from './styled'
+import { Heroe } from '../../../domain/models'
+import { LoadHeroes } from '../../../domain/usecases'
+
+type HomeProps = {
+  loadHeroes: LoadHeroes
+}
+
+export function Home({ loadHeroes }: HomeProps) {
+  const [heroes, setHeroes] = useState<Heroe[]>([])
+
+  useEffect(() => {
+    fetchHeroes()
+  }, [])
+
+  const fetchHeroes = async () => {
+    try {
+      const httpResponse = await loadHeroes.loadAll()
+
+      console.log('httpResponse', httpResponse)
+    } catch (error) {
+      console.log('Error', error)
+    }
+  }
+
   return (
     <Container>
       <Header />
       <Body>
         <Title>Personagens</Title>
-        <List>
-          <Item>Ola</Item>
-          <Item>Ola</Item>
-          <Item>Ola</Item>
-          <Item>Ola</Item>
-          <Item>Ola</Item>
-          <Item>Ola</Item>
-          <Item>Ola</Item>
-          <Item>Ola</Item>
-        </List>
+        <HeroList heroes={heroes} />
       </Body>
     </Container>
   )
