4d2f24e867ad686da7d671d1d9af015a4dc60d18
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-20 16:47:16 +0100

    style: remove code

--- src/presentation/pages/home/styled.ts
+++ src/presentation/pages/home/styled.ts
@@ -24,15 +24,3 @@ export const Title = styled.div`
   text-align: center;
   font-size: 32px;
 `
-
-export const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  grid-gap: 16px;
-  list-style: none;
-  padding: 0;
-`
-
-export const Item = styled.li`
-  background-color: red;
-`
