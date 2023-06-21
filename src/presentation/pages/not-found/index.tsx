5d0470937c33d100816e70768107b7c21952d801
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 13:15:33 +0100

    refactor: add button to back home page

--- src/presentation/pages/not-found/index.tsx
+++ src/presentation/pages/not-found/index.tsx
@@ -1,12 +1,26 @@
-import { Layout, LayoutBody, Title } from '../../components'
-import { Container } from './styled'
+import { useNavigate } from 'react-router-dom'
+
+import { IconHome, Layout, LayoutBody, Title } from '../../components'
+import { Button, Container } from './styled'
+import { useCallback } from 'react'
 
 export function NotFound() {
+	const navigate = useNavigate()
+
+	const handleGoHome = useCallback(() => {
+		navigate('/')
+	}, [navigate])
+
 	return (
 		<Layout title="Página não encontrada | Marvel Heroes">
 			<LayoutBody>
 				<Title>404!</Title>
-				<Container>Página não encontrado!</Container>
+				<Container>
+					Página não encontrado!
+					<Button onClick={handleGoHome}>
+						<span>Ir à página inicial</span> <IconHome />
+					</Button>
+				</Container>
 			</LayoutBody>
 		</Layout>
 	)
