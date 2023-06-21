c87c40b1999e7e7a921bc1f0f3651fff2a7c3fb3
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 15:51:19 +0100

    feat: add navigation to detail on HeroItem component

--- src/presentation/components/hero-item/index.tsx
+++ src/presentation/components/hero-item/index.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Heroe } from '../../../domain/models'
 import { Item, ItemImage, ItemTitle } from './styled'
 
@@ -6,14 +8,21 @@ type HeroItemProps = {
 }
 
 export function HeroItem({ hero }: HeroItemProps) {
+	const navigate = useNavigate()
+
 	const { extension, path } = hero.thumbnail
 	const imgUrl = `${path}/standard_fantastic.${extension}`
 	let imgUnavailable = ''
 	if (imgUrl.includes('image_not_available')) {
 		imgUnavailable = 'true'
 	}
+
+	const handleOpenDetail = useCallback(() => {
+		navigate(`/${hero.id}`)
+	}, [hero.id, navigate])
+
 	return (
-		<Item>
+		<Item onClick={handleOpenDetail}>
 			<ItemImage src={imgUrl} unavailable={imgUnavailable} />
 			<ItemTitle>{hero.name}</ItemTitle>
 		</Item>
