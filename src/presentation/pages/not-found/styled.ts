3ec46b65ecd838c721abeaa35ea4dbd8b50cbffc
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 13:15:08 +0100

    style: NotFound components style

--- src/presentation/pages/not-found/styled.ts
+++ src/presentation/pages/not-found/styled.ts
@@ -1,5 +1,16 @@
 import { styled } from 'styled-components'
 
 export const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 8px;
 	text-align: center;
+`
+export const Button = styled.button`
+	display: flex;
+	gap: 4px;
+	background-color: #0284c7;
+	color: #ffffff;
+	border-radius: 8px;
 `
