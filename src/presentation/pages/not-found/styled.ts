68059d9e8ad068c1b1358ec2d7dad55d51e6dc39
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 12:52:52 +0100

    style: style moved to title component

--- src/presentation/pages/not-found/styled.ts
+++ src/presentation/pages/not-found/styled.ts
@@ -3,10 +3,3 @@ import { styled } from 'styled-components'
 export const Container = styled.div`
 	text-align: center;
 `
-export const Title = styled.div`
-	background-color: #363838;
-	color: #ffffff;
-	padding: 8px;
-	text-align: center;
-	font-size: 32px;
-`
--- src/presentation/pages/search/styled.ts
+++
@@ -1,9 +0,0 @@
-import { styled } from 'styled-components'
-
-export const Title = styled.div`
-	background-color: #363838;
-	color: #ffffff;
-	padding: 8px;
-	text-align: center;
-	font-size: 32px;
-`
