14e3b2a40faacae17fa6c343c19b5dcfda011a1e
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 11:35:29 +0100

    feat: submit search term

--- src/presentation/components/search/index.tsx
+++ src/presentation/components/search/index.tsx
@@ -1,15 +1,32 @@
-import { Container, Input, Button, InputContent } from './styled'
+import { Container, Input, Button, FormContent } from './styled'
+import { useNavigate } from 'react-router-dom'
+
 import { IconSearch } from '..'
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react'
 
 export const SearchHeroes = () => {
+	const navigate = useNavigate()
+	const [query, setQuery] = useState('')
+
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setQuery(e.target.value)
+	}
+
+	const handleSubmit = useCallback(
+		(e: FormEvent) => {
+			e.preventDefault()
+			navigate(`/search/${query}`)
+		},
+		[navigate, query]
+	)
 	return (
 		<Container>
-			<InputContent>
+			<FormContent onSubmit={handleSubmit}>
 				<Button>
 					<IconSearch size={24} />
 				</Button>
-				<Input placeholder="Pesquisar heróis" />
-			</InputContent>
+				<Input placeholder="Pesquisar heróis" onChange={handleInputChange} />
+			</FormContent>
 		</Container>
 	)
 }
