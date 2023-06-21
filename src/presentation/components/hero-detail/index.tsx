61f80817f2a7a76dbaf75a69deace8be55836a7f
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 15:48:18 +0100

    feat: HeroCardDetail component

---
+++ src/presentation/components/hero-detail/index.tsx
@@ -0,0 +1,34 @@
+import { Heroe } from '../../../domain/models'
+import {
+	Container,
+	ImageContainer,
+	Image,
+	BioBody,
+	BioContainer,
+	BioTitle
+} from './styled'
+
+type HeroCardDetailProps = {
+	hero: Heroe
+}
+
+export function HeroCardDetail({ hero }: HeroCardDetailProps) {
+	const { extension, path } = hero.thumbnail
+	const imgUrl = `${path}.${extension}`
+	let imgUnavailable = ''
+	if (imgUrl.includes('image_not_available')) {
+		imgUnavailable = 'true'
+	}
+
+	return (
+		<Container>
+			<ImageContainer>
+				<Image src={imgUrl} unavailable={imgUnavailable} />
+				<BioContainer>
+					<BioTitle>{hero.name}</BioTitle>
+					<BioBody>{hero.description || 'Nenhuma descrição'}</BioBody>
+				</BioContainer>
+			</ImageContainer>
+		</Container>
+	)
+}
