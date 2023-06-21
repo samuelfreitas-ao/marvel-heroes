d28628cda193300237ee924e20be92e6fcadaffb
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 15:48:01 +0100

    style: HeroCardDetail style

---
+++ src/presentation/components/hero-detail/styled.ts
@@ -0,0 +1,38 @@
+import { styled } from 'styled-components'
+
+export const Container = styled.div`
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	min-height: 100px;
+`
+export const ImageContainer = styled.div`
+	display: grid;
+	grid-template-columns: 6fr 4fr;
+	grid-gap: 16px;
+	justify-content: center;
+	align-items: center;
+
+	@media (max-width: 768px) {
+		grid-template-columns: 1fr;
+	}
+`
+export const Image = styled.img<{ unavailable?: string }>`
+	opacity: ${(props) => (props?.unavailable ? 0.6 : 0.8)};
+	box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.5);
+	width: 80%;
+	border-radius: 32px;
+
+	@media (max-width: 1000px) {
+		width: 100%;
+	}
+`
+export const BioContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+	justify-content: center;
+`
+export const BioTitle = styled.h2``
+
+export const BioBody = styled.div``
