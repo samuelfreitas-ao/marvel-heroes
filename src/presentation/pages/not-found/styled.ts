e3c6bf343770e5a8ca93dc3284880480755ea08a
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 13:29:41 +0100

    style: remove button style

--- src/presentation/pages/not-found/styled.ts
+++ src/presentation/pages/not-found/styled.ts
@@ -7,10 +7,3 @@ export const Container = styled.div`
 	gap: 8px;
 	text-align: center;
 `
-export const Button = styled.button`
-	display: flex;
-	gap: 4px;
-	background-color: #0284c7;
-	color: #ffffff;
-	border-radius: 8px;
-`
