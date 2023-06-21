9c36f92787ada5df5c4741880461f25c95c31e82
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-21 12:08:32 +0100

    style: style moved to layout

--- src/presentation/pages/home/styled.ts
+++ src/presentation/pages/home/styled.ts
@@ -1,20 +1,5 @@
 import { styled } from 'styled-components'
 
-export const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-`
-export const Body = styled.main`
-	display: flex;
-	flex-direction: column;
-	gap: 16px;
-	background-color: #ffffff;
-	padding: 24px;
-	border-radius: 32px;
-	margin-top: -80px;
-	margin-left: 24px;
-	margin-right: 24px;
-`
 export const Title = styled.div`
 	background-color: #363838;
 	color: #ffffff;
