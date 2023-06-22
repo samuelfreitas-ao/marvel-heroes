9cfe4613a023fa07101c42f2aae92cf9f0aa95c9
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-22 17:04:41 +0100

    refactor:  correct the type variable name"

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
 	apiUrl: 'https://gateway.marvel.com/v1/public',
-	apiPubliKey: import.meta.env.VITE_PUBLIC_KEY,
+	apiPublicKey: import.meta.env.VITE_PUBLIC_KEY,
 	apiPrivateKey: import.meta.env.VITE_PRIVATE_KEY
 }
--- src/main/factories/http/api-url-factory.ts
+++ src/main/factories/http/api-url-factory.ts
@@ -3,9 +3,9 @@ import CryptoJS from 'crypto-js'
 
 export const makeApiUrl = (path: string) => {
 	const timestamp = new Date().getTime().toString()
-	const dataToHash = `${timestamp}${env.apiPrivateKey}${env.apiPubliKey}`
+	const dataToHash = `${timestamp}${env.apiPrivateKey}${env.apiPublicKey}`
 	const hash = CryptoJS.MD5(dataToHash).toString()
 
-	const url = `${env.apiUrl}${path}?apikey=${env.apiPubliKey}&ts=${timestamp}&hash=${hash}`
+	const url = `${env.apiUrl}${path}?apikey=${env.apiPublicKey}&ts=${timestamp}&hash=${hash}`
 	return url
 }
