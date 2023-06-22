509b3b7dc4f8e68f43ca480e94e54de57adc4b7c
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-22 20:37:32 +0100

    style: fix createGlobalStyle missing type error

--- src/presentation/styles/global.ts
+++ src/presentation/styles/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from 'styled-components'
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle<object>`
 	*{
 		font-family: Exo, Arial, Helvetica, sans-serif;
 		margin: 0;
