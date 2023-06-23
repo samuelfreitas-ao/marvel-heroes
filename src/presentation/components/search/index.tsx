bc9325341c098f3d04861b316149b68c7fb36940
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-23 13:15:14 +0100

    feat: Search on typing

--- src/presentation/components/search/index.tsx
+++ src/presentation/components/search/index.tsx
@@ -2,7 +2,7 @@ import { Container, Input, Button, FormContent } from './styled'
 import { useNavigate } from 'react-router-dom'
 
 import { IconSearch } from '..'
-import { ChangeEvent, FormEvent, useCallback, useState } from 'react'
+import { ChangeEvent, FormEvent, useCallback, useRef, useState } from 'react'
 import { LoadHeroes } from '../../../domain/usecases'
 
 type SearchHeroesProps = {
@@ -11,10 +11,29 @@ type SearchHeroesProps = {
 export const SearchHeroes = ({ loadSearch }: SearchHeroesProps) => {
 	const navigate = useNavigate()
 	const [query, setQuery] = useState('')
+	const timeoutId = useRef<number>()
 
-	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-		setQuery(e.target.value)
-	}
+	const fetchData = useCallback(
+		async (search: string) => {
+			const httpResponse = await loadSearch.loadAll({
+				params: { nameStartsWith: search }
+			})
+			console.log('httpResponse Search', httpResponse)
+		},
+		[loadSearch]
+	)
+
+	const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+		const newSearchQuery = e.target.value
+		setQuery(newSearchQuery)
+
+		clearTimeout(timeoutId.current)
+
+		timeoutId.current = setTimeout(() => {
+			console.log('Realizando pesquisa:', newSearchQuery)
+			fetchData(newSearchQuery)
+		}, 2000)
+	}, [])
 
 	const handleSubmit = useCallback(
 		(e: FormEvent) => {
