7589834800e91d35b4e8e3866d934f5b929da82c
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-06-27 13:12:32 +0100

    style: format pagination elements

--- src/presentation/components/pagination/styled.ts
+++ src/presentation/components/pagination/styled.ts
@@ -2,6 +2,8 @@ import { styled } from 'styled-components'
 
 export const Container = styled.div`
 	margin: 8px;
+	display: flex;
+	gap: 8px;
 `
 export const List = styled.ul`
 	display: flex;
@@ -10,9 +12,13 @@ export const List = styled.ul`
 	list-style: none;
 `
 export const Item = styled.li`
+	display: flex;
+	gap: 2px;
 	padding: 0;
-	border: 1px solid #ddd;
-	padding: 2px 8px;
-	border-radius: 4px;
-	cursor: pointer;
+
+	& button {
+		border: 1px solid #ddd;
+		padding: 2px 8px;
+		border-radius: 4px;
+	}
 `
